1220f01f4a0e324370580772afc7398648b6dfd6
mamulini <mamulini@yahoo.com>
2020-08-11 19:45:22 +0300

    updating shop-page, collection-preview and creating collection-item component, adding some styles

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.scss';
 
 const CollectionPreview = ({title, items}) => (
@@ -7,7 +8,7 @@ const CollectionPreview = ({title, items}) => (
         <div className="preview">
             {items
                 .filter((item, i) => i < 4)
-                .map( item => <div key={item.id}>{item.name}</div>)}
+                .map( ({id, ...otherItemProps}) => <CollectionItem key={id} {...otherItemProps} />)}
         </div>
     </div>
  )
