53a07bcd19dd1d38196e7907763cd1337ad1f46d
mamulini <mamulini@yahoo.com>
2020-08-23 17:08:24 +0300

    Removed redux-logger from producten build

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -23,7 +23,7 @@ const Header = ({ currentUser, hidden }) => (
         SHOP
       </Link>
       <Link to="/shop" className="option">
-        CONTANT
+        CONTACTS
       </Link>
       {currentUser ? (
         <div className="option" onClick={() => auth.signOut()}>
